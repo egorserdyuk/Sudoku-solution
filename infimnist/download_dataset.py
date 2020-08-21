9319d25f4d4810a53eb78beb7d69b27789dbeb02
Egor <egor.serduck@gmail.com>
2020-08-22 00:32:18 +0700

    Update download_dataset.py
    fixed path

--- infimnist/download_dataset.py
+++ infimnist/download_dataset.py
@@ -3,4 +3,4 @@ import urllib.request
 print('Just start me then unzip dataset ;)')
 
 url = 'https://leon.bottou.org/_media/projects/infimnist.tar.gz'
-urllib.request.urlretrieve(url, 'infmnist/infimnist.tar.gz')
+urllib.request.urlretrieve(url, 'infimnist.tar.gz')
